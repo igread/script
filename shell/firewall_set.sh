6ce84631b1a9616f10d621b358408c1d4e0f4c0e
u-on <26396078+u-on@users.noreply.github.com>
2021-05-05 16:41:48 +0800

    ~

--- shell/firewall_set.sh
+++ shell/firewall_set.sh
@@ -5,7 +5,6 @@
 # Depend:       NULL
 #
 
-
 ##################################依赖##################################
 function get_sys() {
     if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
@@ -31,34 +30,76 @@ function get_sys() {
 
 #############################################################################
 
-function firewall_set() {
+firewall_open() {
+    if [ "$(get_sys)" == "CentOS" ]; then
+        systemctl start firewalld
+        systemctl enable firewalld
+        systemctl status firewalld
 
-    if [ "$1" == "off" ]; then
+    elif [ "$(get_sys)" == "Ubuntu" ]; then
+        echo -e "\e[1;31m此操作将中断ssh连接，是否继续？（y|n）\e[0m"
+        ufw enable >/dev/null
+        ufw status
 
-        if [ "$(get_sys)" == "CentOS" ]; then
-            systemctl stop firewalld
-            systemctl disable firewalld
-            systemctl status firewalld
-
-        elif [ "$(get_sys)" == "Ubuntu" ]; then
-            ufw disable
-            ufw status
-        fi
-
-    elif [ "$1" == "on" ]; then
-
-        if [ "$(get_sys)" == "CentOS" ]; then
-            systemctl start firewalld
-            systemctl enable firewalld
-            systemctl status firewalld
-
-        elif [ "$(get_sys)" == "Ubuntu" ]; then
-            echo -e "\e[1;31m此操作将中断ssh连接，是否继续？（y|n）\e[0m"
-            ufw enable >/dev/null
-            ufw status
-
-        fi
     fi
+
 }
 
-firewall_set "$@"
+firewall_close() {
+
+    if [ "$(get_sys)" == "CentOS" ]; then
+        systemctl stop firewalld
+        systemctl disable firewalld
+        systemctl status firewalld
+
+    elif [ "$(get_sys)" == "Ubuntu" ]; then
+        ufw disable
+        ufw status
+    fi
+
+}
+
+
+
+
+while
+    getopts :och OPT
+do
+    case $OPT in
+
+    o)
+        firewall_open
+        ;;
+
+    c)
+        firewall_close
+        ;;
+
+    h)
+        dis=$(
+            cat <<-EOF
+
+\e[1;42m参数：\e[0m
+        -o      # 开启防火墙
+        -c      # 关闭防火墙
+
+ 
+EOF
+        )
+
+        echo -e "${dis}"
+        ;;
+
+    :)      #当选项后面没有参数时，OPT的值被设置为（：），OPTARG的值被设置为选项本身
+        echo "-$OPTARG 选项需要参数" #提示用户此选项后面需要一个参数
+        exit 1
+        ;;
+
+    ?)    #当选项不匹配时，OPT的值被设置为（？），OPTARG的值被设置
+        echo "无效的选项: -$OPTARG" #提示用户此选项无效
+        exit 2
+        ;;
+
+    esac
+
+done
